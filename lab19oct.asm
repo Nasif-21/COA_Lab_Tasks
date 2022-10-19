81aac07f1e8081f2475ba5cfcd99d934abb0887b
Sheikh Muhtasim Nasif <77929303+Nasif-21@users.noreply.github.com>
2022-10-19 09:06:46 +0600

    Print *A*A*
    /',lln kjhbjn;kh;bwr45647868i76o7e7r fimt7fujmlllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNoooooooooooooooooooooooooooooooooppppppppppppppppppppppppppppppppppppppppppppppmkysara mbjaannnnnnnnnnnnnnnnnnnnnnnlooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooujjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuhgggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggguuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii999999999999999999999999999999999999999999999900000000000000000000000-----------------------------------=====================================================================[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]axcscdwsdfwsdsddcIn here, we just print a code  printing A and * but in nikiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiisexkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkiiiiiiiiiiiiiiiixzxssdfdfwfewefwewewedwdqw9qwqwdqwdqwqdwqwqwdqwqwqwqwoqqw0000000000000000000000000000000000000l00000000000--------------------------------------------0ju89=[b yu7rfy de,rutfg0i0ybtg79o7y8r60t7r5rf9obgycdewygfr aegrbikeg far9ioht0erge519

---
+++ lab19oct.asm
@@ -0,0 +1,26 @@
+;.model small
+;.stack 100h
+;.data
+;.code
+;main proc
+   
+;mov cx,4
+;start:
+;mov ah,2
+;mov dl, '*'
+;int 21h
+;mov dl, 10d
+;int 21h
+;mov dl, 13d
+;int 21h
+;mov dl, 'A'
+;int 21h 
+;mov dl, 10d
+;int 21h
+;mov dl, 13d
+;int 21h
+;loop start
+;mov ah, 4ch
+; int 21h
+;main endp
+;end main  
